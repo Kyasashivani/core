cc4c52bde29676a000a6f935ce3683f335d8077f
Kyasashivani <kkyasashivani@gmail.com>
2026-07-06 07:56:49 +0530

    Updated file

--- keywordarguments/example.py
+++ keywordarguments/example.py
@@ -2,3 +2,7 @@ def name(first,middle,last):
     return f"my name is {first} {middle} {last}"
 res=name(last="patel",first="shivani",middle="kyasa")
 print(res)
+def bio(email,age):
+    return f"myemail is {email} and my age is {age}"
+re=bio(age=21,email="shivani@gmail.com")
+print(re)
